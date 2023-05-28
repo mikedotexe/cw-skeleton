b34d7073c2ac0b1172c6a6f71bf93fd40e8539cf
Mike Purvis <mikedotexe@gmail.com>
2023-05-28 10:53:51 -0700

    mods to package manifest, query message, lockfile

--- packages/cw-skeleton-pkg/src/msgs/query_msg.rs
+++ packages/cw-skeleton-pkg/src/msgs/query_msg.rs
@@ -3,6 +3,6 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    #[returns(Config)]
+    #[returns(crate::types::Config)]
     Config {},
 }
